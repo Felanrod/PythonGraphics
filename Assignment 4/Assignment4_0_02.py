5935002d2bb2d9d99c1e57b39bdde420dfa7a114
Joel Murphy <j.murphy2@mycanadore.ca>
2013-07-08 18:36:47 -0400

    Assignment 4 v0.02
    Started working on making the flight more realistic by giving it a delay
    and making the character not move instantly to the mouse position.

---
+++ Assignment 4/Assignment4_0_02.py
@@ -0,0 +1,275 @@
+""" Name: Assignment 4 v0.02
+    Authors: Joel Murphy
+    Date: July 7, 2013
+    Purpose: Side scroller game, collect coins to get points and avoid enemies.
+"""
+    
+import pygame, random
+pygame.init()
+
+screen = pygame.display.set_mode((640, 480))
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("images/character.png")
+        self.image = self.image.convert()
+        self.rect = self.image.get_rect()
+        self.delay = 6
+        self.pause = 0
+        self.dy = 0
+        self.up = -1
+        self.down = 1
+        
+        if not pygame.mixer:
+            print("problem with sound")
+        else:
+            pygame.mixer.init()
+            self.sndYay = pygame.mixer.Sound("yay.ogg")
+            self.sndThunder = pygame.mixer.Sound("thunder.ogg")
+            self.sndEngine = pygame.mixer.Sound("engine.ogg")
+            self.sndEngine.play(-1)
+        
+    def update(self):
+        mousex, mousey = pygame.mouse.get_pos()
+        if mousey != self.rect.centery:
+            self.pause += 1
+            if mousey < self.rect.centery:
+                self.dy = self.up
+            else:
+                self.dy = self.down
+                if self.pause >= 6:
+                    if mousey > screen.get_height() - 31:
+                        self.rect.center = (50, screen.get_height() - 31)
+                    elif mousey < 21:
+                        self.rect.center = (50, 21)
+                    else:
+                        if self.rect.centery > mousey and self.dy > 0:
+                            self.dy = -1
+                        elif self.rect.centery < mousey and self.dy < 0:
+                            self.dy = 1
+                        else:
+                            self.dy = self.dy * 1.5
+                        if 0.5 >= (self.rect.centery + self.dy) - mousey > 0 and -0.5 <= (self.rect.centery + self.dy) - mousey < 0:
+                            self.rect.centery = mousey
+                        else:
+                            self.rect.centery += self.dy
+        else:
+            self.pause = 0
+            self.dy = 0
+                
+class Coin(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("images/coin.png")
+        self.image = self.image.convert()
+        self.rect = self.image.get_rect()
+        self.reset()
+        
+        self.dx = 5
+    
+    def update(self):
+        self.rect.centerx -= self.dx
+        if self.rect.right < 0:
+            self.reset()
+            
+    def reset(self):
+        self.rect.left = screen.get_width()
+        self.rect.centery = random.randrange(10, screen.get_height()-10)
+
+class GroundEnemy(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("images/groundEnemy.png")
+        self.image = self.image.convert()
+        self.rect = self.image.get_rect()
+        self.reset()
+    
+    def update(self):
+        self.rect.centerx -= self.dx
+        if self.rect.right < 0:
+            self.reset()
+    
+    def reset(self):
+        self.rect.bottom = screen.get_height() - 10
+        self.rect.left = screen.get_width()
+        self.dx = random.randrange(5, 10)
+
+class AirEnemy(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("images/airEnemy.png")
+        self.image = self.image.convert()
+        self.rect = self.image.get_rect()
+        self.reset()
+    
+    def update(self):
+        self.rect.centerx -= self.dx
+        if self.rect.right < 0:
+            self.reset()
+    
+    def reset(self):
+        self.rect.bottom = random.randrange(20, screen.get_height() - 30)
+        self.rect.left = screen.get_width()
+        self.dx = random.randrange(5, 10)
+    
+class Ocean(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("ocean.gif")
+        self.rect = self.image.get_rect()
+        self.dy = 5
+        self.reset()
+        
+    def update(self):
+        self.rect.bottom += self.dy
+        if self.rect.bottom >= 1440:
+            self.reset() 
+    
+    def reset(self):
+        self.rect.top = -960
+
+class Scoreboard(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.lives = 5
+        self.score = 0
+        self.font = pygame.font.SysFont("None", 50)
+        
+    def update(self):
+        self.text = "planes: %d, score: %d" % (self.lives, self.score)
+        self.image = self.font.render(self.text, 1, (255, 255, 0))
+        self.rect = self.image.get_rect()
+    
+def game():
+    pygame.display.set_caption("Mail Pilot!")
+
+    background = pygame.Surface(screen.get_size())
+    background.fill((0, 0, 0))
+    screen.blit(background, (0, 0))
+    player = Player()
+    coin = Coin()
+    gEnemy1 = GroundEnemy()
+    gEnemy2 = GroundEnemy()
+    gEnemy3 = GroundEnemy()
+    aEnemy1 = AirEnemy()
+    aEnemy2 = AirEnemy()
+    ocean = Ocean()
+    scoreboard = Scoreboard()
+
+    friendSprites = pygame.sprite.OrderedUpdates(ocean, coin, player)
+    enemySprites = pygame.sprite.Group(gEnemy1, gEnemy2, gEnemy3, aEnemy1, aEnemy2)
+    scoreSprite = pygame.sprite.Group(scoreboard)
+
+    clock = pygame.time.Clock()
+    keepGoing = True
+    while keepGoing:
+        clock.tick(30)
+        pygame.mouse.set_visible(False)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                keepGoing = False
+
+        
+        #check collisions
+        
+        if player.rect.colliderect(coin.rect):
+            player.sndYay.play()
+            coin.reset()
+            scoreboard.score += 100
+
+        hitClouds = pygame.sprite.spritecollide(player, enemySprites, False)
+        if hitClouds:
+            player.sndThunder.play()
+            scoreboard.lives -= 1
+            if scoreboard.lives <= 0:
+                keepGoing = False
+            for theCloud in hitClouds:
+                theCloud.reset()
+        
+        friendSprites.update()
+        enemySprites.update()
+        scoreSprite.update()
+        
+        friendSprites.draw(screen)
+        enemySprites.draw(screen)
+        scoreSprite.draw(screen)
+        
+        pygame.display.flip()
+    
+    player.sndEngine.stop()
+    #return mouse cursor
+    pygame.mouse.set_visible(True) 
+    return scoreboard.score
+    
+def instructions(score):
+    pygame.display.set_caption("Mail Pilot!")
+
+    player = Player()
+    ocean = Ocean()
+    
+    allSprites = pygame.sprite.Group(ocean, player)
+    insFont = pygame.font.SysFont(None, 50)
+    insLabels = []
+    instructions = (
+    "Mail Pilot.     Last score: %d" % score ,
+    "Instructions:  You are a mail pilot,",
+    "delivering mail to the islands.",
+    "",
+    "Fly over an island to drop the mail,",
+    "but be careful not to fly too close",    
+    "to the clouds. Your plane will fall ",
+    "apart if it is hit by lightning too",
+    "many times. Steer with the mouse.",
+    "",
+    "good luck!",
+    "",
+    "click to start, escape to quit..."
+    )
+    
+    for line in instructions:
+        tempLabel = insFont.render(line, 1, (255, 255, 0))
+        insLabels.append(tempLabel)
+ 
+    keepGoing = True
+    clock = pygame.time.Clock()
+    pygame.mouse.set_visible(False)
+    while keepGoing:
+        clock.tick(30)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                keepGoing = False
+                donePlaying = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                keepGoing = False
+                donePlaying = False
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    keepGoing = False
+                    donePlaying = True
+    
+        allSprites.update()
+        allSprites.draw(screen)
+
+        for i in range(len(insLabels)):
+            screen.blit(insLabels[i], (50, 30*i))
+
+        pygame.display.flip()
+        
+    player.sndEngine.stop()    
+    pygame.mouse.set_visible(True)
+    return donePlaying
+        
+def main():
+    donePlaying = False
+    score = 0
+    while not donePlaying:
+        donePlaying = instructions(score)
+        if not donePlaying:
+            score = game()
+
+
+if __name__ == "__main__":
+    main()
+    
+    
